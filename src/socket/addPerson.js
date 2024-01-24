2d0f3347948a4706babf23ecfa550d0593fdcf67
Gurinder <gurinderdhanoa03@gmail.com>
2024-01-24 18:54:38 +0530

    add

--- src/socket/addPerson.js
+++ src/socket/addPerson.js
@@ -2,12 +2,14 @@
 const mongoose = require('mongoose');
 
 module.exports.updatePerson = async(query, update, io, name) => {
+    console.log("=====",update,query);
     mongoose.model("matches").findOneAndUpdate(query, update, { new: true })
         .populate('teamA')
         .populate('teamB')
         .populate('tournament')
         .populate('winning_team')
         .populate('losing_team').then((resdata) => {
+            console.log("resdata resdata",resdata)
             io.emit(name, resdata);
         }).catch((err) => {
             console.log("Error", err);
