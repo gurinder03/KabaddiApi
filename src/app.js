66294028dfe5a566a92f1995761402f358c1d5ba
Gurinder <gurinderdhanoa03@gmail.com>
2024-01-30 18:25:24 +0530

    add

--- src/app.js
+++ src/app.js
@@ -13,9 +13,8 @@ const app = express();
 app.use(cors());
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({ limit: '50MB' }));
 app.use(express.urlencoded({
-    limit: '150mb',
     extended: true
 }))
 
