8aca496f4845bcb498a9d8b0b3412f0bccd42321
Gurinder <gurinderdhanoa03@gmail.com>
2024-02-16 19:36:51 +0530

    add

--- src/socket/tempmatch.js
+++ src/socket/tempmatch.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 
 module.exports.updateTempData = async(update, io,name) => {
-    mongoose.model("tempmatch").findOneAndUpdate({}, update, { new: true })
+    mongoose.model("advertisments").findOneAndUpdate({}, update, { new: true })
     .then((resdata) => {    
         io.emit(name, resdata);
     }).catch((err) => {
