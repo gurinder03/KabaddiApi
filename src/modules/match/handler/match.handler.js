c32f887ef33799bb01f22cf6fe261547bdd1ea70
vikas99144 <vk40010@gmail.com>
2024-02-17 15:56:04 +0530

    add

--- src/modules/match/handler/match.handler.js
+++ src/modules/match/handler/match.handler.js
@@ -74,14 +74,12 @@ module.exports.GETLIST = async (params, callback) => {
     let Collection = params.Collection;
     let aggregateQueryCount = [];
     params.aggregateQuery.map((agg) => {
-            if (!agg.hasOwnProperty('$match') && !agg.hasOwnProperty('$sort') && !agg.hasOwnProperty('$skip') && !agg.hasOwnProperty('$limit')) {
+            if (!agg.hasOwnProperty('$sort') && !agg.hasOwnProperty('$skip') && !agg.hasOwnProperty('$limit')) {
                 aggregateQueryCount.push(agg)
             }
         
     })
-
     aggregateQueryCount.push({ $group: { _id: null, count: { $sum: 1 } } });
-    console.log("///===========", aggregateQueryCount)
     let count = await Collection.aggregate(aggregateQueryCount);
     let totalcount = count.length > 0 ? count[0].count : 0;
     return await Collection
