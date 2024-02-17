75932675deb83dd07fa7f539247b609f9705b66b
vikas99144 <vk40010@gmail.com>
2024-02-17 15:23:14 +0530

    add

--- src/modules/match/handler/match.handler.js
+++ src/modules/match/handler/match.handler.js
@@ -72,9 +72,8 @@ module.exports.DELETE = async (params, callback) => {
 
 module.exports.GETLIST = async (params, callback) => {
     let Collection = params.Collection;
-    let count = await Collection.aggregate(params.aggregateQuery);
-    console.log("///////====",params.aggregateQuery);
-    console.log("== count count ===",count);
+    console.log("==== vdvds",params.aggregateQuery)
+    let count = await Collection.aggregate([{$match:params.obj},{ $group: { _id: null, count: { $sum: 1 } } }]);
     let totalcount = count.length>0?count[0].count:0;
     return await Collection
         .aggregate(params.aggregateQuery)
