4d4fd6da722a8e58d6dc9ab29c10927e6db2d91a
vikas99144 <vk40010@gmail.com>
2024-02-21 21:30:37 +0530

    Add

--- src/modules/match/handler/match.handler.js
+++ src/modules/match/handler/match.handler.js
@@ -110,8 +110,12 @@ module.exports.PUT = async (params, callback) => {
     let Collection = params.Collection;
     let payload = params.payload;
     let query = params.query;
-    console.log("=====/////// params ===", payload);
+    console.log("=====/////// params ===", params);
+    payload.commentator = JSON.parse(JSON.stringify(payload.commentator));
+    payload.refree = JSON.parse(JSON.stringify(payload.refree));
+    payload.chiefguest = JSON.parse(JSON.stringify(payload.commentator));
 
+    console.log("======pp",payload);
 
     return await Collection
         .findOneAndUpdate(query, payload, { new: true })
