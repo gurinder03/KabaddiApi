dbb52c173879c794b248d3a737fcce3efdcfed97
vikas99144 <vk40010@gmail.com>
2024-02-22 21:10:24 +0530

    Add

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -57,6 +57,7 @@ module.exports = function (http, app) {
                 update.is_show_flag = true;
                 update.is_show_refree = false;
                 update.is_show_team = false;
+                update.is_show_player = false;
                 update.is_show_coach = false;
                 update.is_show_chiefguest = false;
                 update.is_show_commentator = false;
